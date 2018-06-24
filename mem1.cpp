68be2e850e0def8e1851daf5f09e4de8988a2830
bluedrop <2899561390@qq.com>
2018-06-24 22:25:10 +0800

    6-24night

--- mem1.cpp
+++ mem1.cpp
@@ -14,12 +14,24 @@ struct mblnode{
 
 mblnode* mbl = NULL;
 
+char BUFF[MAXBUFFER];
+
 int my_init();
 int my_deinit();
 char* my_new(size_t _n);
+int my_print();
+
 
 int main(){
     my_init();
+    int k = 1;
+    my_print();
+    while(true){
+        if(k==0)break;
+        cin >> k;
+        my_new(k);
+        my_print();
+    }
     return 0;
 }
 
@@ -57,20 +69,24 @@ int my_deinit(){
 
 //申请空间
 char* my_new(size_t _n){
+    char* ret = NULL;
+
     if(mbl == NULL)return NULL;
-    if(_n > MAXBUFFER)return "Not Enough Space!";
+    if(_n > MAXBUFFER)return NULL;
     mblnode* p = mbl;
 
+    //查看是否有其余可用剩余块
     for(p=mbl; p->right != mbl;p = p->right){
         if(p->isused || p->size<_n)continue;
         else break;
     }
 
-    if(p == NULL || p->size < _n)return NULL;
+    if(p == NULL || p->size < _n || p->isused)return NULL; //需求空间超出内存，或内存已满
 
     if(p->size > _n){
         //新块
         mblnode* newmbl = new mblnode;
+        p->left->right = newmbl;        //更新前面的指针
         newmbl->size = _n;
         newmbl->isused = true;
         newmbl->left = p->left;
@@ -81,9 +97,31 @@ char* my_new(size_t _n){
         p->size-=_n;
         p->offset +=_n;
         p->left = newmbl;
+
+        //指定分完块后的mbl
+        mbl = p;
+        ret = BUFF + newmbl->offset; //有何用？
     }
     else if(p->size == _n){
         p->isused = true;
-        //在这停顿！！！
+        
+        mbl = p;
+        ret = BUFF + p->offset;
     }
+    return ret;
+}
+
+int my_print(){
+    if(NULL == mbl)return -1;
+    mblnode* p = new mblnode;
+    p = mbl;
+    do{
+        cout << "offset:"<< p->offset << endl;
+        cout << "size:  "<< p->size << endl;
+        cout << "isused:"<< p->isused<< endl;
+        cout << "--------------------" << endl;
+        p=p->right;
+    }while(mbl != p);
+    cout << endl;
+    return 0;
 }
