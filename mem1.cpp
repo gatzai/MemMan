3b90b5f04a37a88e0c2d2bada44da7bac7cbcd40
bluedrop <2899561390@qq.com>
2018-06-24 13:25:25 +0800

    first

---
+++ mem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int k=0;
+    cout << 666 << endl;
+    cin >> k;
+    cout << 666 << endl;
+    cout << 544 << endl;
+    return 0;
+}
